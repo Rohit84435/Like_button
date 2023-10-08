036e69b01448674f74d3cfbd9bdd386f0e484e0b
Rohit kumar <89899254+Rohit84435@users.noreply.github.com>
2023-10-08 19:19:48 +0530

    add all 3 files

---
+++ index.js
@@ -0,0 +1,14 @@
+var card = document.querySelector(".card");
+var icon = document.querySelector("i");
+
+card.addEventListener("dblclick",function(){
+    icon.style.transform = "translate(-50% , -50%) scale(1)";
+    icon.style.opacity = "0.8";
+
+    setTimeout(function(){
+        icon.style.opacity = "0";
+        },1000)
+    setTimeout(function(){
+    icon.style.transform = "translate(-50% , -50%) scale(0)";
+    },2000)
+})
